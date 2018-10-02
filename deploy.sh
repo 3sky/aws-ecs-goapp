cb4c5e4ccc97834776196e937c2ee1b0e7fe3893
3sky <jwolynko@gmail.com>
2018-10-02 18:35:27 +0200

    add next sh script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,5 @@
 # install AWS SDK
-pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
-
-# install ecs-deploy
-add-apt-repository ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
-  sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
-# login AWS ECR
-eval $(aws ecr get-login --region us-east-1)
-
-# or login DockerHub
-# docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
-
-# build the docker image and push to an image repository
-docker build -t goapp .
-docker tag goapp:latest $IMAGE_REPO_URL:latest
 
 # update an AWS ECS service with the new image
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
