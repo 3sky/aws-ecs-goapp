7ca9d35cbd3f67a7fcfc425c7cea6f9577eb3cb5
3sky <jwolynko@gmail.com>
2018-10-02 19:43:15 +0200

    push to ecr fix2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 # install AWS SDK
-pip install --user --upgrade pip
-pip install --user requests==2.5.3
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
@@ -13,12 +11,11 @@ curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-de
 sudo chmod +x /usr/bin/ecs-deploy
 
 # login AWS ECR
-eval $(aws ecr get-login --no-include-email | sed ‘s|https://||’)
+eval $(aws ecr get-login --no-include-email | sed 's|https://||')
 
 # build the docker image and push to an image repository
 docker build -t goapp .
 docker tag goapp:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
 
 # update an AWS ECS service with the new image
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
