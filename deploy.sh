81d39c89a3d1be8ef9417a37bc48e3d9a7fa7cba
3sky <jwolynko@gmail.com>
2018-10-02 19:49:14 +0200

    push to ecr fix3

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ eval $(aws ecr get-login --no-include-email | sed 's|https://||')
 # build the docker image and push to an image repository
 docker build -t goapp .
 docker tag goapp:latest $IMAGE_REPO_URL:latest
+docker push $IMAGE_REPO_URL:latest
 
 # update an AWS ECS service with the new image
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
