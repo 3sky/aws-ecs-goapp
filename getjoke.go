46785ab928bc0c619a3c0ee885c163ac8e77fba6
3sky <jwolynko@gmail.com>
2018-10-04 06:48:39 +0200

    add proper JSON format

--- getjoke.go
+++ getjoke.go
@@ -14,25 +14,48 @@ import (
 	"github.com/gorilla/mux"
 )
 
+//JokeStruct struct
+type JokeStruct struct {
+	ID   int    `json:"id,omitempty"`
+	JOKE string `json:"joke,omitempty"`
+}
+
+//HelloStruct for valid JSON
+type HelloStruct struct {
+	ID  int    `json:"id,omitempty"`
+	SAY string `json:"text,omitempty"`
+}
+
+//CheckStruct checks
+type CheckStruct struct {
+	ID  int    `json:"id,omitempty"`
+	SAY string `json:"text,omitempty"`
+}
+
+var j []JokeStruct
+var h []HelloStruct
+var c []CheckStruct
+
 //GetJoke gets jokes
 func GetJoke(w http.ResponseWriter, r *http.Request) {
+	j := &JokeStruct{ID: 1, JOKE: DownloadJoke()}
 	enc := json.NewEncoder(w)
 	enc.SetEscapeHTML(false)
-	_ = enc.Encode(DownloadJoke())
-
+	_ = enc.Encode(j)
 }
 
 //SayHello seyhello
 func SayHello(w http.ResponseWriter, r *http.Request) {
-	hmgs := "Hello Scalac"
-	json.NewEncoder(w).Encode(hmgs)
+	h := &HelloStruct{ID: 1, SAY: "Hello Scalac"}
+	json.NewEncoder(w).Encode(h)
 }
 
 //Check will work on `/`
 func Check(w http.ResponseWriter, r *http.Request) {
 	dt := time.Now()
 	cmsg := fmt.Sprintf("It work at: %s", dt.String())
-	json.NewEncoder(w).Encode(cmsg)
+	h := &CheckStruct{ID: 1, SAY: cmsg}
+	json.NewEncoder(w).Encode(h)
 }
 
 //DownloadJoke get from bash.org.pl/random
